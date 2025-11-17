53f363aae20acbf8cf903cb69a418df77038c513
Albert <albdanvic@alu.edu.gva>
2025-11-17 20:23:45 +0100

    Error solucionado

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipController;
 
-
+// Ruta de bienvenida (GET /)
 Route::get('/', fn() => "Benvingut a la Guia d'Equips de Futbol Femení!");
 
 // Genera automáticamente varias rutas REST para 'equips'
